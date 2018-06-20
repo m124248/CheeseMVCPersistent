caf683c961ba3d179f6068bd2d3bc1d97162d8ab
m124248 <cluning08@gmail.com>
2018-06-19 20:10:14 -0500

    End of Class 6/19

---
+++ src/CheeseMVC/Controllers/CategoryController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CheeseMVC.Models;
+using System.Collections.Generic;
+using CheeseMVC.ViewModels;
+using CheeseMVC.Data;
+using System.Linq;
+
+namespace CheeseMVC.Controllers
+{
+    public class CategoryController : Controller 
+    {
+        private readonly CheeseDbContext context;
+
+        public CategoryController(CheeseDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            var allCategories = context.Categories.ToList();
+            return View(allCategories);
+        }
+        
+        public IActionResult Add()
+        {
+            AddCategoryViewModel addCategoryViewModel = new AddCategoryViewModel();
+            return View(addCategoryViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddCategoryViewModel addCategoryViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                // Add the new category to my existing category
+                CheeseCategory newCategory = new CheeseCategory
+                {
+                    Name = addCategoryViewModel.Name,
+                };
+
+                context.Categories.Add(newCategory);
+                context.SaveChanges();
+
+                return Redirect("/Category");
+            }
+
+            return View(addCategoryViewModel);
+        }
+    }
+}
---
+++ src/CheeseMVC/Models/CheeseCategory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheeseMVC.Models
+{
+    public class CheeseCategory
+    {
+        public string Name { get; set; }
+    }
+}
